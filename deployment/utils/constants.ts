8cef0389265bb0a4fbc9b3f05c1ee3b664c6a05c
alenabelium <33514602+alenabelium@users.noreply.github.com>
2024-01-16 23:52:27 +0100

    mock indexer adaptation

---
+++ deployment/utils/constants.ts
@@ -0,0 +1,3 @@
+export const LAST_CHAIN_INDEX_STATE = "last_chain_block";
+export const LAST_DATABASE_INDEX_STATE = "last_database_block";
+export const FIRST_DATABASE_INDEX_STATE = "first_database_block";
