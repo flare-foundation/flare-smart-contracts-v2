8b5248d6d3a97a7cfc55f4072f445ac93fe8fc11
Bostjan Kovac <bostjan.kovac@abelium.com>
2024-02-20 08:44:17 +0100

    update docs

--- contracts/userInterfaces/IRandomProvider.sol
+++ contracts/userInterfaces/IRandomProvider.sol
@@ -14,14 +14,16 @@ interface IRandomProvider {
     function getCurrentRandom() external view returns(uint256 _randomNumber);
 
     /**
-     * Returns current random number with quality.
+     * Returns current random number and a flag indicating if it was securely generated.
+     * It is up to the caller to decide whether to use the returned random number or not.
      * @return _randomNumber Current random number.
      * @return _isSecureRandom Indicates if current random number is secure.
      */
     function getCurrentRandomWithQuality() external view returns(uint256 _randomNumber, bool _isSecureRandom);
 
     /**
-     * Returns current random number with quality.
+     * Returns current random number, a flag indicating if it was securely generated and its timestamp.
+     * It is up to the caller to decide whether to use the returned random number or not.
      * @return _randomNumber Current random number.
      * @return _isSecureRandom Indicates if current random number is secure.
      * @return _randomTimestamp Random timestamp.
