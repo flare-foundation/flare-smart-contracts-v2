800405c8d6fa607a20ab62099e45bed91c32d07c
Bostjan Kovac <bostjan.kovac@abelium.com>
2024-02-26 09:48:28 +0100

    fix permanent data staleness

--- test-forge/ftso/implementation/FtsoFeedPublisher.t.sol
+++ test-forge/ftso/implementation/FtsoFeedPublisher.t.sol
@@ -133,6 +133,32 @@ contract FtsoFeedPublisherTest is Test {
         ftsoFeedPublisher.getFeed(feedName2, 2);
     }
 
+    function testPublishVotingRoundTooHigh() public {
+        uint32 roundId = 2000;
+        _mockGetVotingRoundId(block.timestamp, 4);
+        IFtsoFeedPublisher.FeedWithProof[] memory proofs = new IFtsoFeedPublisher.FeedWithProof[](2);
+        bytes32[] memory merkleProof1 = new bytes32[](1);
+        bytes32[] memory merkleProof2 = new bytes32[](1);
+
+        IFtsoFeedPublisher.Feed memory body1 = IFtsoFeedPublisher.Feed(
+            roundId, feedName1, int32(100), uint16(1000), int8(2));
+        IFtsoFeedPublisher.Feed memory body2 = IFtsoFeedPublisher.Feed(
+            roundId, feedName2, int32(200), uint16(2000), int8(3));
+        bytes32 leaf1 = keccak256(abi.encode(body1));
+        bytes32 leaf2 = keccak256(abi.encode(body2));
+        bytes32 merkleRoot = _hashPair(leaf1, leaf2);
+
+        merkleProof1[0] = leaf2;
+        proofs[0] = IFtsoFeedPublisher.FeedWithProof(merkleProof1, body1);
+
+        merkleProof2[0] = leaf1;
+        proofs[1] = IFtsoFeedPublisher.FeedWithProof(merkleProof2, body2);
+
+        _mockGetMerkleRoot(4, roundId, merkleRoot);
+        vm.expectRevert("voting round id too high");
+        ftsoFeedPublisher.publish(proofs);
+    }
+
     function testPublishRevertInvalidProof() public {
         uint32 roundId = 2;
         _mockGetVotingRoundId(block.timestamp, 4);
@@ -195,6 +221,7 @@ contract FtsoFeedPublisherTest is Test {
         assertEq(getFeed.decimals, int8(2));
 
         // move to voting round 15
+        _mockGetVotingRoundId(block.timestamp, 15);
         roundId = 12;
         IFtsoFeedPublisher.Feed memory feed = IFtsoFeedPublisher.Feed(
             roundId, feedName1, int32(8), uint16(18), int8(13));
@@ -211,30 +238,46 @@ contract FtsoFeedPublisherTest is Test {
         assertEq(getFeed.decimals, int8(13));
 
         // feed for voting round 2 was overwritten by feed for voting round 12 (history size is 10)
-        vm.expectRevert("feed not published yet");
+        vm.expectRevert("too old voting round id");
         getFeed = ftsoFeedPublisher.getFeed(feedName1, 2);
 
         // publish again feed for voting round 2
-        roundId = 2;
         feed = IFtsoFeedPublisher.Feed(
-            roundId, feedName1, int32(100), uint16(1000), int8(2));
+            2, feedName1, int32(100), uint16(1000), int8(2));
         feeds = new IFtsoFeedPublisher.Feed[](1);
         feeds[0] = feed;
         vm.prank(feedsPublisher);
         ftsoFeedPublisher.publishFeeds(feeds);
 
+        // feed for voting round 12 should not be overritten
+        vm.expectRevert("too old voting round id");
         getFeed = ftsoFeedPublisher.getFeed(feedName1, 2);
-        assertEq(getFeed.votingRoundId, 2);
-        assertEq(getFeed.name, feedName1);
-        assertEq(getFeed.value, int32(100));
-        assertEq(getFeed.turnoutBIPS, uint16(1000));
-        assertEq(getFeed.decimals, int8(2));
 
-        // feed for voting round 12 was overwritten by feed for voting round 2 (history size is 10)
-        vm.expectRevert("feed not published yet");
         getFeed = ftsoFeedPublisher.getFeed(feedName1, 12);
+        assertEq(getFeed.votingRoundId, roundId);
+        assertEq(getFeed.name, feedName1);
+        assertEq(getFeed.value, int32(8));
+        assertEq(getFeed.turnoutBIPS, uint16(18));
+        assertEq(getFeed.decimals, int8(13));
     }
 
+    function testPublishFeedsVotingRoundTooHigh() public {
+        testSetFeedsPublisher();
+        _mockGetVotingRoundId(block.timestamp, 4);
+        uint32 roundId = 2000;
+        IFtsoFeedPublisher.Feed memory feed1 = IFtsoFeedPublisher.Feed(
+            roundId, feedName1, int32(100), uint16(1000), int8(2));
+        IFtsoFeedPublisher.Feed memory feed2 = IFtsoFeedPublisher.Feed(
+            roundId, feedName2, int32(200), uint16(2000), int8(3));
+
+        IFtsoFeedPublisher.Feed[] memory feeds = new IFtsoFeedPublisher.Feed[](2);
+        feeds[0] = feed1;
+        feeds[1] = feed2;
+
+        vm.prank(feedsPublisher);
+        vm.expectRevert("voting round id too high");
+        ftsoFeedPublisher.publishFeeds(feeds);
+    }
 
     //// helper functions
     function _mockGetVotingRoundId(uint256 _blockTs, uint256 _roundId) private {
