c2cf60c6eb06e8b951c00c550aa568ee74a2bd0c
Timotej Vesel <timotej.vesel@gmail.com>
2024-08-20 18:19:25 +0200

    feeCalculator add tests

--- test-forge/unit/fastUpdates/implementation/FeeCalculator.t.sol
+++ test-forge/unit/fastUpdates/implementation/FeeCalculator.t.sol
@@ -16,6 +16,8 @@ contract FeeCalculatorTest is Test {
 
     bytes21[] private feedIds;
 
+    uint256[] private indices8 = new uint256[](8);
+
     event FeedFeeSet(bytes21 indexed feedId, uint256 fee);
     event CategoryFeeSet(uint8 indexed category, uint256 fee);
     event FeedFeeRemoved(bytes21 indexed feedId);
@@ -46,6 +48,10 @@ contract FeeCalculatorTest is Test {
         vm.stopPrank();
 
         _setFeedIds();
+
+        for (uint256 i = 0; i < 8; i++) {
+            indices8[i] = i;
+        }
     }
 
     function testSetDefaultFee() public {
@@ -194,113 +200,166 @@ contract FeeCalculatorTest is Test {
         feeCalculator.getCategoryFee(1);
     }
 
-    // function testCalculateFee1() public {
-    //     // no default fees set (i.e. all are 0), no overrides set
-    //     uint256[] memory indices = new uint256[](8);
-    //     for (uint256 i = 0; i < 8; i++) {
-    //         indices[i] = i;
-    //     }
-    //     assertEq(feeCalculator.calculateFee(indices), 0);
-    // }
+    function testCalculateFee1() public {
+        // no fees set, default fee is used
 
-    // function testCalculateFee2() public {
-    //     // set default fee for group 1 to 10 and for group 2 to 20
-    //     uint8[] memory categories = new uint8[](2);
-    //     categories[0] = 1;
-    //     categories[1] = 2;
-    //     uint256[] memory fees = new uint256[](2);
-    //     fees[0] = 10;
-    //     fees[1] = 20;
-    //     vm.prank(governance);
-    //     feeCalculator.setCategoriesDefaultFees(categories, fees);
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 8 * 10);
 
-    //     // no overrides set
-    //     uint256[] memory indices = new uint256[](8);
-    //     for (uint256 i = 0; i < 8; i++) {
-    //         indices[i] = i;
-    //     }
-    //     assertEq(feeCalculator.calculateFee(indices), 10 * 2 + 20 * 3 + 0 * 3);
-    // }
+        // change default fee to 1
+        vm.prank(governance);
+        feeCalculator.setDefaultFee(1);
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 8 * 1);
+    }
 
-    // function testCalculateFee3() public {
-    //     // set default fee for group 1 to 10 and for group 2 to 20
-    //     uint8[] memory categories = new uint8[](2);
-    //     categories[0] = 1;
-    //     categories[1] = 2;
-    //     uint256[] memory fees = new uint256[](2);
-    //     fees[0] = 10;
-    //     fees[1] = 20;
-    //     vm.prank(governance);
-    //     feeCalculator.setCategoriesDefaultFees(categories, fees);
+    function testCalculateFee2() public {
+        // set fee for group 1 to 100 and for group 2 to 200
+        uint8[] memory categories = new uint8[](2);
+        categories[0] = 1;
+        categories[1] = 2;
+        uint256[] memory fees = new uint256[](2);
+        fees[0] = 100;
+        fees[1] = 200;
+        vm.prank(governance);
+        feeCalculator.setCategoriesFees(categories, fees);
 
-    //     // override fee for feed0 and feed2 to 0
-    //     uint256[] memory feedFees = new uint256[](2);
-    //     feedFees[0] = 0;
-    //     feedFees[1] = 0;
-    //     bytes21[] memory feedIdsToSet = new bytes21[](2);
-    //     feedIdsToSet[0] = feedIds[0];
-    //     feedIdsToSet[1] = feedIds[2];
-    //     vm.prank(governance);
-    //     feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 100 * 2 + 200 * 3 + 10 * 3);
+    }
 
-    //     uint256[] memory indices = new uint256[](8);
-    //     for (uint256 i = 0; i < 8; i++) {
-    //         indices[i] = i;
-    //     }
+    function testCalculateFee3() public {
+        // set fee for group 1 to 100 and for group 2 to 200
+        uint8[] memory categories = new uint8[](2);
+        categories[0] = 1;
+        categories[1] = 2;
+        uint256[] memory fees = new uint256[](2);
+        fees[0] = 100;
+        fees[1] = 200;
+        vm.prank(governance);
+        feeCalculator.setCategoriesFees(categories, fees);
 
-    //     assertEq(feeCalculator.calculateFee(indices), 10 * 1 + 20 * 2 + 0 * 5);
-    // }
+        // set feed fee for feed0 and feed2 to 0
+        uint256[] memory feedFees = new uint256[](2);
+        feedFees[0] = 0;
+        feedFees[1] = 0;
+        bytes21[] memory feedIdsToSet = new bytes21[](2);
+        feedIdsToSet[0] = feedIds[0];
+        feedIdsToSet[1] = feedIds[2];
+        vm.prank(governance);
+        feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
 
-    // function testCalculateFee4() public {
-    //     // default fee for all groups is 0
-    //     uint8[] memory categories = new uint8[](1);
-    //     categories[0] = 3;
-    //     uint256[] memory fees = new uint256[](1);
-    //     fees[0] = 1000;
-    //     vm.prank(governance);
-    //     feeCalculator.setCategoriesDefaultFees(categories, fees);
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 100 * 1 + 200 * 2 + 10 * 3 + 0 * 2);
+    }
 
-    //     // set (override) fee for feeds feed0-feed6
-    //     uint256[] memory feedFees = new uint256[](7);
-    //     bytes21[] memory feedIdsToSet = new bytes21[](7);
-    //     for (uint256 i = 0; i < 7; i++) {
-    //         feedFees[i] = (i + 1) * 10;
-    //         feedIdsToSet[i] = feedIds[i];
+    function testCalculateFee4() public {
+        // set fee for group 3 to 1000
+        uint8[] memory categories = new uint8[](1);
+        categories[0] = 3;
+        uint256[] memory fees = new uint256[](1);
+        fees[0] = 1000;
+        vm.prank(governance);
+        feeCalculator.setCategoriesFees(categories, fees);
 
-    //     }
-    //     vm.prank(governance);
-    //     feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
+        // set feed fee for feeds feed0-feed6
+        uint256[] memory feedFees = new uint256[](7);
+        bytes21[] memory feedIdsToSet = new bytes21[](7);
+        for (uint256 i = 0; i < 7; i++) {
+            feedFees[i] = (i + 1) * 10;
+            feedIdsToSet[i] = feedIds[i];
 
-    //     uint256[] memory indices = new uint256[](8);
-    //     for (uint256 i = 0; i < 8; i++) {
-    //         indices[i] = i;
-    //     }
+        }
+        vm.prank(governance);
+        feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
 
-    //     assertEq(feeCalculator.calculateFee(indices), 10 + 20 + 30 + 40 + 50 + 60 + 70 + 1000);
-    // }
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 10 + 20 + 30 + 40 + 50 + 60 + 70 + 1000);
+    }
 
-    // function testCalculateFee5() public {
-    //     // default fee for all groups 1 and 2 is 0, for group 3 1000
+    function testCalculateFee5() public {
+        // set fee for all groups to 0
+        uint8[] memory categories = new uint8[](3);
+        uint256[] memory fees = new uint256[](3);
+        for (uint8 i = 0; i < 3; i++) {
+            fees[i] = 0;
+            categories[i] = i + 1;
+        }
+        vm.prank(governance);
+        feeCalculator.setCategoriesFees(categories, fees);
 
+        // set feed fee for feeds feed0-feed6
+        uint256[] memory feedFees = new uint256[](7);
+        bytes21[] memory feedIdsToSet = new bytes21[](7);
+        for (uint256 i = 0; i < 7; i++) {
+            feedFees[i] = (i + 1) * 10;
+            feedIdsToSet[i] = feedIds[i];
+        }
+        vm.prank(governance);
+        feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
 
-    //     // set (override) fee for feeds feed0-feed6
-    //     uint256[] memory feedFees = new uint256[](7);
-    //     bytes21[] memory feedIdsToSet = new bytes21[](7);
-    //     for (uint256 i = 0; i < 7; i++) {
-    //         feedFees[i] = (i + 1) * 10;
-    //         feedIdsToSet[i] = feedIds[i];
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 10 + 20 + 30 + 40 + 50 + 60 + 70 + 0);
+    }
 
-    //     }
-    //     vm.prank(governance);
-    //     feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
+    function testCalculateFee6() public {
+        // set fee for all groups to 0
+        uint8[] memory categories = new uint8[](3);
+        uint256[] memory fees = new uint256[](3);
+        for (uint8 i = 0; i < 3; i++) {
+            fees[i] = 0;
+            categories[i] = i + 1;
+        }
+        vm.prank(governance);
+        feeCalculator.setCategoriesFees(categories, fees);
 
-    //     uint256[] memory indices = new uint256[](8);
-    //     for (uint256 i = 0; i < 8; i++) {
-    //         indices[i] = i;
-    //     }
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 0);
+    }
 
-    //     assertEq(feeCalculator.calculateFee(indices), 10 + 20 + 30 + 40 + 50 + 60 + 70 + 0);
-    // }
+    function testCalculateFee7() public {
+        // set fee for all groups to 19
+        uint8[] memory categories = new uint8[](3);
+        categories[0] = 1;
+        categories[1] = 2;
+        categories[2] = 3;
+        uint256[] memory fees = new uint256[](3);
+        fees[0] = 19;
+        fees[1] = 19;
+        fees[2] = 19;
+        vm.prank(governance);
+        feeCalculator.setCategoriesFees(categories, fees);
+
+        // set feed fee for all feeds to 0
+        uint256[] memory feedFees = new uint256[](8);
+        bytes21[] memory feedIdsToSet = new bytes21[](8);
+        for (uint256 i = 0; i < 8; i++) {
+            feedFees[i] = 0;
+            feedIdsToSet[i] = feedIds[i];
+        }
+        vm.prank(governance);
+        feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
+
+        assertEq(feeCalculator.calculateFeeByIndices(indices8), 0);
+    }
+
+    function testCalculateFeeByIds() public {
+        // set fee for category 1 to 100
+        uint8[] memory categories = new uint8[](1);
+        categories[0] = 1;
+        uint256[] memory fees = new uint256[](1);
+        fees[0] = 100;
+        vm.prank(governance);
+        feeCalculator.setCategoriesFees(categories, fees);
+
+        // set fee for feed1 to 900
+        uint256[] memory feedFees = new uint256[](1);
+        feedFees[0] = 900;
+        bytes21[] memory feedIdsToSet = new bytes21[](1);
+        feedIdsToSet[0] = feedIds[1];
+        vm.prank(governance);
+        feeCalculator.setFeedsFees(feedIdsToSet, feedFees);
+
+        bytes21[] memory feedIdsToCalculate = new bytes21[](3);
+        feedIdsToCalculate[0] = feedIds[0];
+        feedIdsToCalculate[1] = feedIds[1];
+        feedIdsToCalculate[2] = feedIds[2];
+
+        assertEq(feeCalculator.calculateFeeByIds(feedIdsToCalculate), 100 + 900 + 10);
+    }
 
     ////
     function _getFeedId(uint8 _category, string memory _name) internal pure returns(bytes21) {
