41b8c5981f10127a138378fb880dce244bfbfe01
Bostjan Kovac <bostjan.kovac@abelium.com>
2025-06-18 09:35:12 +0200

    add fdcProtocolId and relay method to interface

--- contracts/userInterfaces/IFdcVerification.sol
+++ contracts/userInterfaces/IFdcVerification.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.6 <0.9;
 
+import "./IRelay.sol";
 import "./fdc/IAddressValidityVerification.sol";
 import "./fdc/IBalanceDecreasingTransactionVerification.sol";
 import "./fdc/IConfirmedBlockHeightExistsVerification.sol";
@@ -19,4 +20,14 @@ interface IFdcVerification is
     IEVMTransactionVerification,
     IPaymentVerification,
     IReferencedPaymentNonexistenceVerification
-{ }
+{
+    /**
+     * The FDC protocol id.
+     */
+    function fdcProtocolId() external view returns (uint8 _fdcProtocolId);
+
+    /**
+     * Relay contract address.
+     */
+    function relay() external view returns (IRelay);
+}
