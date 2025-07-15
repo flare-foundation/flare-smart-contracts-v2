ce30d0053a81ebed5a2dd8d40ad0ad76752c8642
Bostjan Kovac <bostjan.kovac@abelium.com>
2025-07-15 14:24:29 +0200

    rename verifyJsonApi to verifyWeb2Json + redeploy

--- contracts/userInterfaces/fdc/IWeb2JsonVerification.sol
+++ contracts/userInterfaces/fdc/IWeb2JsonVerification.sol
@@ -5,6 +5,6 @@ import { IWeb2Json } from "./IWeb2Json.sol";
 
 interface IWeb2JsonVerification {
 
-    function verifyJsonApi(IWeb2Json.Proof calldata _proof)
+    function verifyWeb2Json(IWeb2Json.Proof calldata _proof)
         external view returns (bool _proved);
 }
