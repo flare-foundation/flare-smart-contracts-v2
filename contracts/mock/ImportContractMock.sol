4b48914d15183cab7d1cc2da42a4bd5f7224c7a5
Timotej Vesel <timotej.vesel@gmail.com>
2026-04-13 22:34:42 +0200

    chore: fix solhint warnings in mock contracts
    Add solhint-disable for intentional global import in ImportContractMock.
    Remove unused IPChainStakeMirrorVerifier and WNat imports.

--- contracts/mock/ImportContractMock.sol
+++ contracts/mock/ImportContractMock.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
+// solhint-disable-next-line no-global-import
 import "../../flattened/FlareSmartContracts.sol";
 
 // should never be use - just
--- contracts/mock/PChainStakeMirrorVerifier.sol
+++ contracts/mock/PChainStakeMirrorVerifier.sol
@@ -7,9 +7,6 @@ import {
 import {
     IPChainStakeMirrorMultiSigVoting
 } from "@flarenetwork/flare-periphery-contracts/flare/IPChainStakeMirrorMultiSigVoting.sol";
-import {
-    IPChainStakeMirrorVerifier
-} from "@flarenetwork/flare-periphery-contracts/flare/IPChainStakeMirrorVerifier.sol";
 import { IRelay } from "../userInterfaces/IRelay.sol";
 import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
