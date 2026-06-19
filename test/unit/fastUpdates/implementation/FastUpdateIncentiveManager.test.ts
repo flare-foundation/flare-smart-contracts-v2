98423f0506fc1bb379f4eaa6deac76ca017af594
Bostjan Kovac <bostjan@flare.network>
2026-06-19 16:20:47 +0200

    test(fastUpdates): format givenMethodReturn gas option

--- test/unit/fastUpdates/implementation/FastUpdateIncentiveManager.test.ts
+++ test/unit/fastUpdates/implementation/FastUpdateIncentiveManager.test.ts
@@ -401,7 +401,9 @@ contract(`FastUpdateIncentiveManager.sol; ${getTestFile(__filename)}`, (accounts
       ["tuple(bytes21,uint32,uint24)[]"], //  IFastUpdatesConfiguration.FeedConfiguration (bytes21 feedId, uint32 rewardBandValue, uint24 inflationShare)
       [configs]
     );
-    await fastUpdatesConfiguration.givenMethodReturn(getFeedConfigurations, getFeedConfigurationsReturn, {gas: 125000000});
+    await fastUpdatesConfiguration.givenMethodReturn(getFeedConfigurations, getFeedConfigurationsReturn, {
+      gas: 125000000,
+    });
 
     expect(await fastUpdateIncentiveManager.getContractName()).to.equal("FastUpdateIncentiveManager");
 
