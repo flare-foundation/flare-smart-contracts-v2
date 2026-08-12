8adaaf9450c854df565e6e0e88cdba19725dbe01
alenabelium <alen@abelium.eu>
2026-08-12 10:40:18 +0200

    fix(build): correct Solidity compatibility pragmas

--- contracts/userInterfaces/IFixedPointArithmetic.sol
+++ contracts/userInterfaces/IFixedPointArithmetic.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.6 <0.9;
+// User-defined value types require Solidity 0.8.8 or newer.
+pragma solidity >=0.8.8 <0.9;
 
 /*
  * Opaque type synonyms to enforce arithemtic correctness.
